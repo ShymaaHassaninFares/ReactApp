fbfcaad776282f626f859af69f09a19f611dc250
ShymaaHassaninFares <shymaa.hassanin@itworx.com>
2023-12-25 10:19:43 +0200

    Add files via upload

---
+++ Product API/Product.DomainModel/MapResponseObject.cs
@@ -0,0 +1,16 @@
+﻿using AutoWrapper;
+
+namespace Product.Domain.Model
+{
+    public class MapResponseObject
+    {
+        [AutoWrapperPropertyMap(Prop.ResponseException)]
+        public object Error { get; set; }
+
+        [AutoWrapperPropertyMap(Prop.ResponseException_ExceptionMessage)]
+        public string Message { get; set; }
+
+        [AutoWrapperPropertyMap(Prop.ResponseException_Details)]
+        public string StackTrace { get; set; }
+    }
+}
---
+++ Product API/Product.DomainModel/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Product.Domain.Model
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+    }
+}
